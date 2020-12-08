d150d7f4295e559c8069de601994b4691ef74e9e
Neeraj Kumar <neerajchaudhary0699@gmail.com>
2020-12-09 00:41:28 +0530

    Modify Procfile-3

--- Fetch_Url_Content/Fetch_Url_Content/settings.py
+++ Fetch_Url_Content/Fetch_Url_Content/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '5jx^&cr#=yp+r4^g=cpk-ep1m)a3)ry7zrkkfil)fojrox&q^z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
